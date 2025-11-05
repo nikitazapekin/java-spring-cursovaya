12ee3a9ebed287f793baeb465ad00de59eb0ec38
Global Name <zapekinnikitamain@gmail.com>
2025-11-05 18:27:29 +0300

    feat(card): Medical card

--- src/main/java/com/example/medicalapp/service/JwtService.java
+++ src/main/java/com/example/medicalapp/service/JwtService.java
@@ -19,7 +19,7 @@ public class JwtService {
     @Value("${jwt.access.expiration:30000}")
     private Long accessExpiration;
 
-    @Value("${jwt.refresh.expiration:60000}")
+    @Value("${jwt.refresh.expiration:6000000}")
     private Long refreshExpiration;
 
     public String generateAccessToken(String email, String role, String userType) {
